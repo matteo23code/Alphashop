94f3481ef455059763f111b6af2bb3150750bc79
Matteo Basile <73886056+matteo23code@users.noreply.github.com>
2021-06-21 14:50:04 +0200

    add data services

--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticoliDataService } from '../services/data/articoli-data.service';
 
-export class articoli {
+export class Articoli {
 
   constructor(
     public codart: string,
@@ -23,16 +24,20 @@ export class articoli {
 })
 export class ArticoliComponent implements OnInit {
 
-  articoli = [
+  NumArt = 0;
+
+ /*  articoli = [
     new articoli('014600301', 'BARILLA FARINA 1 KG', 'PZ', 24, 1, 1.09, true, new Date()),
     new articoli('013500121', 'BARILLA PASTA GR.500 N.70 1/2 PENNE', 'PZ', 24, 1, 1.09, true, new Date()),
     new articoli("007686402", "FINDUS FIOR DI NASELLO 300 GR", "PZ", 8, 0.3, 6.46, true, new Date()),
     new articoli("057549001", "FINDUS CROCCOLE 400 GR", "PZ", 12, 0.4, 5.97,true, new Date())
-  ]
+  ] */
 
-  constructor() { }
+  constructor(private articoliService: ArticoliDataService) { }
 
   ngOnInit(): void {
+    
+    
   }
 
 }
---
+++ src/app/services/data/articoli-data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Articoli } from 'src/app/articoli/articoli.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticoliDataService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  
+  getArticoli(descrizione : string) {
+
+    return this.httpClient.get<Articoli[]>(`http://localhost:5051/api/articoli/cerca/cerca/descrizione/${descrizione}`);
+  }
+}
